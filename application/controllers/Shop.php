d896f02b9def82733056a068e73c000cff54130f
dev <dev@develop.com>
2020-02-09 18:40:19 +0530

    changes

--- application/controllers/Shop.php
+++ application/controllers/Shop.php
@@ -92,7 +92,7 @@ class Shop extends CI_Controller {
                 $this->email->from(email_bcc, $sendername);
                 $this->email->to($this->input->post('email'));
                 $this->email->bcc(email_bcc);
-                $subjectt = "Thanks you for joining";
+                $subjectt = "Thank you for joining";
                 $subject = $subjectt;
                 $this->email->subject($subject);
                 $appointment['appointment'] = $web_input;
@@ -147,7 +147,7 @@ class Shop extends CI_Controller {
                 $this->email->from(email_bcc, $sendername);
                 $this->email->to($this->input->post('email'));
                 $this->email->bcc(email_bcc);
-                $subjectt = "Thanks you for joining";
+                $subjectt = "Thank you for interest in Baan Thai";
                 $subject = $subjectt;
                 $this->email->subject($subject);
                 $appointment['appointment'] = $web_input;
@@ -428,92 +428,8 @@ class Shop extends CI_Controller {
         $this->load->view('Pages/reviews');
     }
 
-    public function stylingTips() {
-        $query = $this->db->get('style_tips');
-        $data['stylebook'] = $query->result_array();
-        $this->load->view('Pages/stylebook', $data);
-    }
+  
 
-    public function stylingTipsTag() {
-        $tag = $this->input->get('tag');
-        $this->db->where("tag like '%$tag%'");
-        $query = $this->db->get('style_tips');
-
-        $tagblock = $query->result_array();
-
-        $data['stylebook'] = $tagblock;
-
-        $this->load->view('Pages/stylebook', $data);
-    }
-
-    function styleTipsDetails($style_index, $title) {
-
-        if ($this->session_user) {
-            $checklogin = true;
-        } else {
-            $checklogin = false;
-        }
-
-        if (isset($_POST['submit'])) {
-            $blogdata = array(
-                "title" => $this->input->post('title'),
-                "description" => $this->input->post('description')
-            );
-            $this->db->set($blogdata);
-            $this->db->where('id', $style_index); //set column_name and value in which row need to update
-            $this->db->update('style_tips');
-            redirect("styleTips/" . $style_index . "/" . $title);
-        }
-
-
-        $data['checklogin'] = $checklogin;
-
-
-        $this->db->where('id', $style_index);
-        $query = $this->db->get('style_tips');
-
-        $styleobj = $query->row();
-        $data['styleobj'] = $styleobj;
-
-        $configuration = $this->config->load('seo_config');
-
-//$seotitle_o = $this->config->item("seo_title");
-
-        $seotitle1 = "Hong Kong Bespoke Tailors | " . $styleobj->title;
-        $seodescription = $styleobj->description;
-
-        $this->config->set_item('seo_title', $seotitle1);
-        $this->config->set_item('seo_desc', $seodescription);
-
-
-        $this->db->from('style_tips');
-        $this->db->order_by("id", "desc");
-        $this->db->limit(5);
-        $query = $this->db->get();
-        $stylebook = $query->result_array();
-
-
-        $data['stylebook'] = $stylebook;
-
-        $this->db->from('style_tips');
-        $this->db->order_by("id", "desc");
-        $query = $this->db->get();
-        $stylebook1 = $query->result_array();
-
-        $tagarray1 = array();
-        foreach ($stylebook1 as $key => $value) {
-            $tags = $value['tag'];
-            $tagarray = explode(", ", $tags);
-            foreach ($tagarray as $key1 => $value1) {
-                $tagarray1[$value1] = [];
-            }
-        }
-
-        $data['tagsarray'] = $tagarray1;
-
-
-
-        $this->load->view('Pages/stylebookdeails', $data);
-    }
+  
 
 }
