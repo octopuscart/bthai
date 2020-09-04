a088dd5235c116226dd2f96fda4eaa1cdfebcb6d
dev <dev@develop.com>
2020-09-04 09:05:40 +0530

    changes

--- application/views/home.php
+++ application/views/home.php
@@ -20,7 +20,8 @@
         <!-- Stylesheets
         ============================================= -->
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i" rel="stylesheet" type="text/css" />
-        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/theme/css/bootstrap.css" type="text/css" />
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
         <link rel="stylesheet" href="<?php echo base_url(); ?>assets/theme/css/style.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo base_url(); ?>assets/theme/css/dark.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo base_url(); ?>assets/theme/css/font-icons.css" type="text/css" />
